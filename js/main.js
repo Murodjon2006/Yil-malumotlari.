f89999215cbbc4df0c88f164af3fb863fafcf8c7
MurodjonHatamov <hatamovmurodjon1@gmail.com>
2025-02-23 20:29:11 +1200

    yangilandi

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const yil = document.querySelector(".yil");
 const kun = document.querySelector(".kun");
 const Chiqarish = document.querySelector(".Chiqarish");
 const boxs = document.querySelector(".boxs");
+const body = document.querySelector("body");
 
 
 Chiqarish.addEventListener("click", () => {
@@ -10,44 +11,54 @@ Chiqarish.addEventListener("click", () => {
   const oyValue = select.value;
   const kunValue = kun.value;
 
+  if (!yilValue || !oyValue || !kunValue) {
+    alert("Iltimos, barcha ma'lumotlarni kiriting!");
+    return;
+  }
 
-if (!yilValue || !oyValue || !kunValue) {
-  alert("Iltimos, barcha ma'lumotlarni kiriting!");
-  return;
-}
+  if (!/^\d+$/.test(yilValue) || yilValue > 9999) {
+    alert("Yil faqat raqamlardan iborat bo‘lishi kerak!");
+    return;
+  } else if (!/^\d+$/.test(kunValue) || kunValue > 31) {
+    alert("Kun faqat raqamlardan iborat bo‘lishi kerak!");
+    return;
+  }
 
-if (!/^\d+$/.test(yilValue) || yilValue>9999) {
-  alert("Yil faqat raqamlar bo'lishi kerak!");
-  return;
-} else if (!/^\d+$/.test(kunValue) || kunValue>31) {
-  alert("Kun faqat raqamlar bo'lishi kerak!");
-  return;
-}
-
-  const yosh = new Date().getFullYear() - yilValue-1;
+  const yosh = new Date().getFullYear() - yilValue - 1;
   const kunlar = Math.floor((Date.now() - new Date(yilValue, select.selectedIndex, kunValue).getTime()) / (1000 * 60 * 60 * 24));
   const kabisaYillar = Math.floor(yosh / 4);
+  
+  const hayvonYil = ["Sichqon", "Sigir", "Yo'lbars", "Quyon", "Baliq", 
+    "Ilon", "Ot", "Qo'y", "Maymun", "Tovuq", "It", "To'ng'iz"];
+  const yilHayvon = hayvonYil[(yilValue - 4) % 12];
 
-  const hayvonYil = ["Sichqon", "Sigir", "Yo'lbars", "Quyon", "Ilon", "Ot", "Qo'y", "Maymun", "Xo'roz", "It", "To'ng'iz"];
-  const yilHayvon = hayvonYil[(yilValue - 5) % 12];
 
-  boxs.innerHTML = `
-    <h2>Sizning yil ma'lumotlaringiz</h2>
-    <div class="box">
-        <h3 class="sanoq">1</h3>
-        <p>Siz <span>${yosh}</span> yoshdasiz.</p>
-    </div>
-    <div class="box">
-        <h3 class="sanoq">2</h3>
-        <p>Tug'ilganingizga <span>${kunlar}</span> kun bo'ldi.</p>
-    </div>
-    <div class="box">
-        <h3 class="sanoq">3</h3>
-        <p>Siz <span>${yilHayvon}</span> yilida tug'ilgansiz.</p>
-    </div>
-    <div class="box">
-        <h3 class="sanoq">4</h3>
-        <p> <span>${kabisaYillar}</span> marta kabisa yili o'tdi.</p>
-    </div>
-  `;
+  if (boxs.innerHTML.trim() === "") {
+    body.classList.add("activ");
+    setTimeout(() => {
+      body.classList.remove("activ");
+    }, 1000);
+  }
+
+ 
+  setTimeout(() => {
+    boxs.innerHTML = `
+      <div class="box">
+          <h3 class="sanoq">1</h3>
+          <p>Siz <span>${yosh}</span> yoshdasiz.</p>
+      </div>
+      <div class="box">
+          <h3 class="sanoq">2</h3>
+          <p>Tug‘ilganingizga <span>${kunlar}</span> kun bo‘ldi.</p>
+      </div>
+      <div class="box">
+          <h3 class="sanoq">3</h3>
+          <p>Siz <span>${yilHayvon}</span> yilida tug‘ilgansiz.</p>
+      </div>
+      <div class="box">
+          <h3 class="sanoq">4</h3>
+          <p> <span>${kabisaYillar}</span> marta kabisa yili o‘tdi.</p>
+      </div>
+    `;
+  }, 1000);
 });
